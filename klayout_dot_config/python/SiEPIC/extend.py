d6e4e19e2ef2f318b52b7cfac8156c3d90aefeb8
Mustafa Hammood <mustafa@ece.ubc.ca>
2020-08-02 14:12:16 -0700

    refactor path classes extensions out of siepic.extend

--- klayout_dot_config/python/SiEPIC/extend.py
+++ klayout_dot_config/python/SiEPIC/extend.py
@@ -71,179 +71,19 @@ warning.setDefaultButton(pya.QMessageBox.Ok)
 # Function Definitions
 #################################################################################
 
-
-def to_dtype(self, dbu):
-    Dpath = pya.DPath(self.get_dpoints(), self.width) * dbu
-    Dpath.width = self.width * dbu
-    return Dpath
-
-
-def to_itype(self, dbu):
-    path = pya.Path([pt.to_itype(dbu) for pt in self.each_point()], round(self.width / dbu))
-    path.width = round(self.width / dbu)
-    return path
-
-
-def get_points(self):
-    return [pya.Point(pt.x, pt.y) for pt in self.each_point()]
-
-
-def get_dpoints(self):
-    return [pya.DPoint(pt.x, pt.y) for pt in self.each_point()]
-
-
-def is_manhattan_endsegments(self):
-    if self.__class__ == pya.Path:
-        pts = self.get_points()
-    else:
-        pts = self.get_dpoints()
-    check = 1 if len(pts) == 2 else 0
-    for i, pt in enumerate(pts):
-        if (i == 1 or pts[i] == pts[-1]):
-            if(pts[i].x == pts[i - 1].x or pts[i].y == pts[i - 1].y):
-                check += 1
-    return check == 2
-
-
-def is_manhattan(self):
-    if self.__class__ == pya.Path:
-        pts = self.get_points()
-    else:
-        pts = self.get_dpoints()
-    if len(pts) == 2:
-        return True
-        
-    # check that each segment is horizontal or vertical:
-    for i, pt in enumerate(pts[0:-1]):
-        if not (pts[i].x == pts[i + 1].x or pts[i].y == pts[i + 1].y):
-            return False
-
-    # check that all corners are 90 degrees (not 180):
-    from SiEPIC.utils import inner_angle_b_vectors
-    for i in range(1, len(pts)-1):
-        if ((inner_angle_b_vectors(pts[i]-pts[i-1],pts[i+1]-pts[i])+90)%360-90) != 90:
-            return False
- 
-    return True
-
-
-def radius_check(self, radius):
-    def all2(iterable):
-        for element in iterable:
-            if not element:
-                return False
-        return True
-
-    points = self.get_points()
-    
-    if len(points) > 2:
-    
-      lengths = [points[i].distance(points[i - 1]) for i, pt in enumerate(points) if i > 0]
-  
-      # first and last segment must be >= radius
-      check1 = (lengths[0] >= radius)
-      check2 = (lengths[-1] >= radius)
-      # middle segments must accommodate two bends, hence >= 2 radius
-      check3 = [length >= 2 * radius for length in lengths[1:-1]]
-      return check1 and check2 and all(check3)
-    else:
-      return True
-
-# remove all colinear points (only keep corners)
-def remove_colinear_points(self, verbose=False):
-    from .utils import pt_intersects_segment, angle_b_vectors
-    if self.__class__ == pya.Path:
-        pts = self.get_points()
-    else:
-        pts = self.get_dpoints()
-
-    for i in range(1,len(pts)-1):
-      turn = ((angle_b_vectors(pts[i]-pts[i-1],pts[i+1]-pts[i])+90)%360-90)/90
-      angle = angle_vector(pts[i]-pts[i-1])/90
-      if verbose:
-        print('%s, %s' %(turn, angle))
-
-
-    # this version removed all colinear points, which doesn't make sense for a path
-    self.points = [pts[0]] + [pts[i]
-                              for i in range(1, len(pts) - 1) if not pt_intersects_segment(pts[i + 1], pts[i - 1], pts[i])] + [pts[-1]]
-    return self
-
-
-def unique_points(self):
-    if self.__class__ == pya.Path:
-        pts = self.get_points()
-    else:
-        pts = self.get_dpoints()
-
-    # only keep unique path points:
-    output = []
-    for pt in pts:
-        if pt not in output:
-            output.append(pt)
-    self.points = output
-    return self
-
-
-def translate_from_center(self, offset):
-    from math import pi, cos, sin, acos, sqrt
-    from .utils import angle_vector
-    pts = [pt for pt in self.get_dpoints()]
-    tpts = [pt for pt in self.get_dpoints()]
-    for i in range(0, len(pts)):
-        if i == 0:
-            u = pts[i] - pts[i + 1]
-            v = -u
-        elif i == (len(pts) - 1):
-            u = pts[i - 1] - pts[i]
-            v = -u
-        else:
-            u = pts[i - 1] - pts[i]
-            v = pts[i + 1] - pts[i]
-
-        if offset < 0:
-            o1 = pya.DPoint(abs(offset) * cos(angle_vector(u) * pi / 180 - pi / 2),
-                            abs(offset) * sin(angle_vector(u) * pi / 180 - pi / 2))
-            o2 = pya.DPoint(abs(offset) * cos(angle_vector(v) * pi / 180 + pi / 2),
-                            abs(offset) * sin(angle_vector(v) * pi / 180 + pi / 2))
-        else:
-            o1 = pya.DPoint(abs(offset) * cos(angle_vector(u) * pi / 180 + pi / 2),
-                            abs(offset) * sin(angle_vector(u) * pi / 180 + pi / 2))
-            o2 = pya.DPoint(abs(offset) * cos(angle_vector(v) * pi / 180 - pi / 2),
-                            abs(offset) * sin(angle_vector(v) * pi / 180 - pi / 2))
-
-        p1 = u + o1
-        p2 = o1
-        p3 = v + o2
-        p4 = o2
-        d = (p1.x - p2.x) * (p3.y - p4.y) - (p1.y - p2.y) * (p3.x - p4.x)
-
-        if round(d, 10) == 0:
-            tpts[i] += p2
-        else:
-            tpts[i] += pya.DPoint(((p1.x * p2.y - p1.y * p2.x) * (p3.x - p4.x) - (p1.x - p2.x) * (p3.x * p4.y - p3.y * p4.x)) / d,
-                                  ((p1.x * p2.y - p1.y * p2.x) * (p3.y - p4.y) - (p1.y - p2.y) * (p3.x * p4.y - p3.y * p4.x)) / d)
-
-    if self.__class__ == pya.Path:
-        return pya.Path([pya.Point(pt.x, pt.y) for pt in tpts], self.width)
-    elif self.__class__ == pya.DPath:
-        return pya.DPath(tpts, self.width)
-
-'''
-snap - pya.Path extension
-This function snaps the two path endpoints to the nearest pins by adjusting the end segments
-
-Input:
- - self: the Path object
- - pins: an array of Pin objects, which are paths with 2 points,
-         with the vector giving the direction (out of the component)
-Output:
- - modifies the original Path
-
-'''
-
-
 def snap(self, pins):
+    '''
+    snap - pya.Path extension
+    This function snaps the two path endpoints to the nearest pins by adjusting the end segments
+
+    Input:
+    - self: the Path object
+    - pins: an array of Pin objects, which are paths with 2 points,
+            with the vector giving the direction (out of the component)
+    Output:
+    - modifies the original Path
+
+    '''
     # Import functionality from SiEPIC-Tools:
     from .utils import angle_vector, get_technology
     from . import _globals
@@ -379,21 +219,19 @@ def snap(self, pins):
       return False
       
 
-'''
-snap_m - pya.Path extension
-This function snaps the two metal path endpoints to the nearest metal pins by adjusting the end segments
-Author: Karl McNulty, RIT Integrated Photonics Group
-Input:
- - self: the Path object
- - pins: an array of Pin objects, which are paths with 2 points,
-         with the vector giving the direction (out of the component)
-Output:
- - modifies the original Path
-
-'''
-
-
 def snap_m(self, pins):
+    '''
+    snap_m - pya.Path extension
+    This function snaps the two metal path endpoints to the nearest metal pins by adjusting the end segments
+    Author: Karl McNulty, RIT Integrated Photonics Group
+    Input:
+    - self: the Path object
+    - pins: an array of Pin objects, which are paths with 2 points,
+            with the vector giving the direction (out of the component)
+    Output:
+    - modifies the original Path
+
+    '''
     # Import functionality from SiEPIC-Tools:
     from .utils import angle_vector, get_technology
     from . import _globals
@@ -515,34 +353,13 @@ def snap_m(self, pins):
 
 # Path Extension
 #################################################################################
-
-pya.Path.to_dtype = to_dtype
-pya.Path.to_itype = to_itype
-pya.Path.get_points = get_points
-pya.Path.get_dpoints = get_dpoints
-pya.Path.is_manhattan_endsegments = is_manhattan_endsegments
-pya.Path.is_manhattan = is_manhattan
-pya.Path.radius_check = radius_check
-pya.Path.remove_colinear_points = remove_colinear_points
-pya.Path.unique_points = unique_points
-pya.Path.translate_from_center = translate_from_center
-pya.Path.snap = snap
-pya.Path.snap_m = snap_m # function added by Karl McNulty (RIT) for metal pin functionality
+pya.Path.snap = snap # gui only
+pya.Path.snap_m = snap_m # gui only, function added by Karl McNulty (RIT) for metal pin functionality
 
 # DPath Extension
 #################################################################################
-
-pya.DPath.to_itype = to_itype
-pya.DPath.to_dtype = to_dtype
-pya.DPath.get_points = get_points
-pya.DPath.get_dpoints = get_dpoints
-pya.DPath.is_manhattan_endsegments = is_manhattan_endsegments
-pya.DPath.is_manhattan = is_manhattan
-pya.DPath.radius_check = radius_check
-pya.DPath.remove_colinear_points = remove_colinear_points
-pya.DPath.unique_points = unique_points
-pya.DPath.translate_from_center = translate_from_center
-pya.DPath.snap = snap
+pya.DPath.snap = snap # gui only
+pya.DPath.snap_m = snap_m # gui only, function added by Karl McNulty (RIT) for metal pin functionality
 
 #################################################################################
 #            SiEPIC Class Extension of Polygon & DPolygon Class                 #
---
+++ klayout_dot_config/python/siepic_tools/extend/path.py
@@ -0,0 +1,192 @@
+#################################################################################
+#                SiEPIC Class Extension of Path & DPath Class                   #
+#################################################################################
+
+# Function Definitions
+#################################################################################
+'''
+pya.Path and pya.DPath Extensions:
+  - get_points(), returns list of pya.Points
+  - get_dpoints(), returns list of pya.DPoints
+  - is_manhattan(), tests to see if the path is manhattan
+  - is_manhattan_endsegments(), tests to see if the path is manhattan (only the 1st and last segments)
+  - radius_check(radius), tests to see of all path segments are long enough to be
+    converted to a waveguide with bends of radius 'radius'
+  - remove_colinear_points(), removes all colinear points in place
+  - unique_points(), remove all but one colinear points
+  - translate_from_center(offset), returns a new path whose points have been offset
+    by 'offset' from the center of the original path
+  - snap(pins), snaps the path in place to the nearest pin
+'''
+import pya
+
+
+def patch_path(Path_Klass):
+    def to_dtype(self, dbu):
+        Dpath = pya.DPath(self.get_dpoints(), self.width) * dbu
+        Dpath.width = self.width * dbu
+        return Dpath
+
+    def to_itype(self, dbu):
+        path = pya.Path([pt.to_itype(dbu) for pt in self.each_point()], round(self.width / dbu))
+        path.width = round(self.width / dbu)
+        return path
+
+    def get_points(self):
+        return [pya.Point(pt.x, pt.y) for pt in self.each_point()]
+
+    def get_dpoints(self):
+        return [pya.DPoint(pt.x, pt.y) for pt in self.each_point()]
+
+    def is_manhattan_endsegments(self):
+        if self.__class__ == pya.Path:
+            pts = self.get_points()
+        else:
+            pts = self.get_dpoints()
+        check = 1 if len(pts) == 2 else 0
+        for i, pt in enumerate(pts):
+            if (i == 1 or pts[i] == pts[-1]):
+                if(pts[i].x == pts[i - 1].x or pts[i].y == pts[i - 1].y):
+                    check += 1
+        return check == 2
+
+
+    def is_manhattan(self):
+        if self.__class__ == pya.Path:
+            pts = self.get_points()
+        else:
+            pts = self.get_dpoints()
+        if len(pts) == 2:
+            return True
+            
+        # check that each segment is horizontal or vertical:
+        for i, pt in enumerate(pts[0:-1]):
+            if not (pts[i].x == pts[i + 1].x or pts[i].y == pts[i + 1].y):
+                return False
+
+        # check that all corners are 90 degrees (not 180):
+        from SiEPIC.utils import inner_angle_b_vectors
+        for i in range(1, len(pts)-1):
+            if ((inner_angle_b_vectors(pts[i]-pts[i-1],pts[i+1]-pts[i])+90)%360-90) != 90:
+                return False
+        return True
+
+    def radius_check(self, radius):
+        def all2(iterable):
+            for element in iterable:
+                if not element:
+                    return False
+            return True
+
+        points = self.get_points()
+        
+        if len(points) > 2:
+            lengths = [points[i].distance(points[i - 1]) for i, pt in enumerate(points) if i > 0]
+            # first and last segment must be >= radius
+            check1 = (lengths[0] >= radius)
+            check2 = (lengths[-1] >= radius)
+            # middle segments must accommodate two bends, hence >= 2 radius
+            check3 = [length >= 2 * radius for length in lengths[1:-1]]
+            return check1 and check2 and all(check3)
+        else:
+            return True
+
+            
+    def remove_colinear_points(self, verbose=False):
+        """ remove all but 1 colinear point """
+        from .utils import pt_intersects_segment, angle_b_vectors
+        if self.__class__ == pya.Path:
+            pts = self.get_points()
+        else:
+            pts = self.get_dpoints()
+
+        for i in range(1,len(pts)-1):
+            turn = ((angle_b_vectors(pts[i]-pts[i-1],pts[i+1]-pts[i])+90)%360-90)/90
+            angle = angle_vector(pts[i]-pts[i-1])/90
+            if verbose:
+                print('%s, %s' %(turn, angle))
+
+        # this version removed all colinear points, which doesn't make sense for a path
+        self.points = [pts[0]] + [pts[i]
+                                for i in range(1, len(pts) - 1) if not pt_intersects_segment(pts[i + 1], pts[i - 1], pts[i])] + [pts[-1]]
+        return self
+
+
+    def unique_points(self):
+        if self.__class__ == pya.Path:
+            pts = self.get_points()
+        else:
+            pts = self.get_dpoints()
+
+        # only keep unique path points:
+        output = []
+        for pt in pts:
+            if pt not in output:
+                output.append(pt)
+        self.points = output
+        return self
+
+
+    def translate_from_center(self, offset):
+        from math import pi, cos, sin, acos, sqrt
+        from .utils import angle_vector
+        pts = [pt for pt in self.get_dpoints()]
+        tpts = [pt for pt in self.get_dpoints()]
+        for i in range(0, len(pts)):
+            if i == 0:
+                u = pts[i] - pts[i + 1]
+                v = -u
+            elif i == (len(pts) - 1):
+                u = pts[i - 1] - pts[i]
+                v = -u
+            else:
+                u = pts[i - 1] - pts[i]
+                v = pts[i + 1] - pts[i]
+
+            if offset < 0:
+                o1 = pya.DPoint(abs(offset) * cos(angle_vector(u) * pi / 180 - pi / 2),
+                                abs(offset) * sin(angle_vector(u) * pi / 180 - pi / 2))
+                o2 = pya.DPoint(abs(offset) * cos(angle_vector(v) * pi / 180 + pi / 2),
+                                abs(offset) * sin(angle_vector(v) * pi / 180 + pi / 2))
+            else:
+                o1 = pya.DPoint(abs(offset) * cos(angle_vector(u) * pi / 180 + pi / 2),
+                                abs(offset) * sin(angle_vector(u) * pi / 180 + pi / 2))
+                o2 = pya.DPoint(abs(offset) * cos(angle_vector(v) * pi / 180 - pi / 2),
+                                abs(offset) * sin(angle_vector(v) * pi / 180 - pi / 2))
+
+            p1 = u + o1
+            p2 = o1
+            p3 = v + o2
+            p4 = o2
+            d = (p1.x - p2.x) * (p3.y - p4.y) - (p1.y - p2.y) * (p3.x - p4.x)
+
+            if round(d, 10) == 0:
+                tpts[i] += p2
+            else:
+                tpts[i] += pya.DPoint(((p1.x * p2.y - p1.y * p2.x) * (p3.x - p4.x) - (p1.x - p2.x) * (p3.x * p4.y - p3.y * p4.x)) / d,
+                                     ((p1.x * p2.y - p1.y * p2.x) * (p3.y - p4.y) - (p1.y - p2.y) * (p3.x * p4.y - p3.y * p4.x)) / d)
+
+        if self.__class__ == pya.Path:
+            return pya.Path([pya.Point(pt.x, pt.y) for pt in tpts], self.width)
+        elif self.__class__ == pya.DPath:
+            return pya.DPath(tpts, self.width)
+
+
+    pya.Path_Klass.to_dtype = to_dtype
+    pya.Path_Klass.to_itype = to_itype
+    pya.Path_Klass.get_points = get_points
+    pya.Path_Klass.get_dpoints = get_dpoints
+    pya.Path_Klass.is_manhattan_endsegments = is_manhattan_endsegments
+    pya.Path_Klass.is_manhattan = is_manhattan
+    pya.Path_Klass.radius_check = radius_check
+    pya.Path_Klass.remove_colinear_points = remove_colinear_points
+    pya.Path_Klass.unique_points = unique_points
+    pya.Path_Klass.translate_from_center = translate_from_center
+    #pya.Path.snap = snap
+    #pya.Path.snap_m = snap_m # function added by Karl McNulty (RIT) for metal pin functionality
+
+# Path Extension
+patch_path(pya.Path)
+
+# DPath Extension
+patch_path(pya.DPath)
