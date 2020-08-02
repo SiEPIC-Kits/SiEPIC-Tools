9f37937e0b68f74b950968db152255d6c5f0cdef
Mustafa Hammood <mustafa@ece.ubc.ca>
2020-08-02 15:20:21 -0700

    refactor sampling into siepic_tools.utils

--- klayout_dot_config/python/siepic_tools/utils/__init__.py
+++ klayout_dot_config/python/siepic_tools/utils/__init__.py
@@ -4,6 +4,7 @@ Utilities module for siepic_tools
 
 from .tech import get_technology_by_name  # noqa
 from .tech import register_siepic_technology  # noqa
+from . import sampling
 
 # The code here in utils require certain klayout extensions.
 import siepic_tools.extend  # noqa
