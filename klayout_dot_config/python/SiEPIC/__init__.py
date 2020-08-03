9f28d9f5def695487891182c1cfae15279cd9d80
Mustafa Hammood <mustafa@ece.ubc.ca>
2020-08-02 20:28:53 -0700

    add operation tag in siepic intialization

--- klayout_dot_config/python/SiEPIC/__init__.py
+++ klayout_dot_config/python/SiEPIC/__init__.py
@@ -1,5 +1,18 @@
 __version__ = '0.4'
 #from . import install, extend, _globals, core, examples, github, lumerical, scripts, utils, setup
 # from . import install, extend, _globals, core, examples, github, scripts, utils, setup
+op_tag = "" #operation tag which defines whether we are loading library in script or GUI env
+
+try:
+    import pya
+    if "Application" in str(dir(pya)):
+        from SiEPIC.utils import get_technology_by_name
+        op_tag = "GUI" 
+    else:
+        op_tag = "script"
+except ImportError:
+    import klayout.db as pya
+    op_tag = "script"
+
 from . import extend, _globals, core, examples, github, scripts, utils, setup, install
 from siepic_tools import *
