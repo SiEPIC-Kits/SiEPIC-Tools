2f1baa7177d54fe7523e85dba19c0fc2dcfc5f18
Mustafa Hammood <mustafa@ece.ubc.ca>
2020-08-04 18:21:41 -0700

    gc_shunt example clean up

--- Examples/script_layouts/gc_shunt/gc_shunt.py
+++ Examples/script_layouts/gc_shunt/gc_shunt.py
@@ -3,6 +3,9 @@ cd_siepic = r"C:\Users\mhammood\Documents\GitHub\SiEPIC-Tools2\klayout_dot_confi
 cd_pdk = r"C:\Users\mhammood\Documents\GitHub\SiEPIC-Tools2\klayout_dot_config\tech\GSiP"
 pdk = 'GSiP'
 
+layer_Si220 = 'Si'
+layer_floorplan = 'FloorPlan'
+layer_text = 'Text'
 #%% initialize imports
 import sys, os
 sys.path.append(cd_siepic); sys.path.append(cd_pdk)
@@ -24,13 +27,9 @@ lib = get_technology_by_name(pdk, cd_pdk)
 ly = pya.Layout()
 dbu = ly.dbu = 0.001
 cell_top = ly.create_cell("Top")
-
 ly.prune_subcells(cell_top.cell_index(), 1000)
-#%%Define Layer mapping and floor plan
-layer_Si220 = 'Si'
-layer_floorplan = 'FloorPlan'
-layer_text = 'Text'
 
+#%%Define Layer mapping and floor plan
 LayerSiN = ly.layer(lib[layer_Si220])
 fpLayerN = cell_top.layout().layer(lib[layer_floorplan])
 TextLayerN = cell_top.layout().layer(lib[layer_text])
@@ -41,29 +40,27 @@ cell_top.shapes(fpLayerN).insert(Box(0,0, ly_width/dbu, ly_height/dbu))
 
 #%%Import Grating couplers
 GC_imported = ly.create_cell("Grating_Coupler_13deg_TE_1550_Oxide", pdk).cell_index()
-gc_length = 41
 GC_pitch = 127
 t = Trans(Trans.R0, 0.5*ly_width/dbu, (0.5*ly_height-GC_pitch/2)/dbu)
 cell_top.insert(CellInstArray(GC_imported, t, DPoint(0,GC_pitch).to_itype(dbu), Point(0,0), 2, 1))
 
 #%%draw waveguide connecting grating couplers
-path_pts = [[0.5*ly_width,0.5*ly_height-GC_pitch/2], [0.5*ly_width+50,0.5*ly_height-GC_pitch/2], [0.5*ly_width+50, 0.5*ly_height+GC_pitch/2], [0.5*ly_width,0.5*ly_height+GC_pitch/2]]
-path_pts = DPath([DPoint(each[0], each[1]) for each in path_pts],0.5)
-path_pts = path_pts.to_itype(dbu)
-
-from siepic_tools.utils.layout import layout_waveguide, layout_waveguide2
-#layout_waveguide(cell_top, LayerSiN, path_pts, 0.5)
+path = [[0.5*ly_width,0.5*ly_height-GC_pitch/2]] # start point
+path.append([0.5*ly_width+50,0.5*ly_height-GC_pitch/2])
+path.append([0.5*ly_width+50, 0.5*ly_height+GC_pitch/2])
+path.append([[0.5*ly_width,0.5*ly_height+GC_pitch/2]]) # end point
+path = DPath([DPoint(each[0], each[1]) for each in path],0.5)
+path = path.to_itype(dbu)
+pts = path.get_points()
 
 widths = [0.5]
 layers = ['Waveguide']
 offset = [0]
 radius = 15
 
-pts = path_pts.get_points()
-a = layout_waveguide2(lib, ly, cell_top, layers, widths, offset, pts, radius, False,0)
+from siepic_tools.utils.layout import layout_waveguide2
+layout_waveguide2(lib, ly, cell_top, layers, widths, offset, pts, radius, False,0)
 
 cd_save = r"C:\Users\mhammood\Documents\GitHub\SiEPIC-Tools2\Examples\script_layouts\gc_shunt"
 os.chdir(cd_save)
 ly.write("gc_shunt.gds")
-
-    # %%
